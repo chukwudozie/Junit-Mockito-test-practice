bf50bf1d771af08dec4be9742e042d0d5eff7207
chukwudozie <emkychuks062@gmail.com>
2022-03-08 10:43:22 +0100

    remove the power mock and use Mockito for static class

--- src/test/java/emeka/powermock/MockingStaticMethodTest.java
+++ src/test/java/emeka/powermock/MockingStaticMethodTest.java
@@ -3,16 +3,22 @@ package emeka.powermock;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+//import org.powermock.api.mockito.PowerMockito;
+//import org.powermock.core.classloader.annotations.PowerMockIgnore;
+//import org.powermock.core.classloader.annotations.PrepareForTest;
+//import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
-
 public class MockingStaticMethodTest {
 
     @Rule
@@ -30,13 +36,13 @@ public class MockingStaticMethodTest {
 //    }
 
     @Test
-    public void test(){
+    public void testMethodCallingStaticMethod_UsingMock(){
 
-        List<Integer> allStats = Arrays.asList(1,2);
+        List<Integer> allStats = Arrays.asList(1,2,3);
         when(dependency.retrieveAllStats()).thenReturn(allStats);
+        Mockito.mockStatic(UtilityClass.class);
+        when(UtilityClass.staticMethod(6)).thenReturn(150);
         test.methodCallingAStaticMethod();
-
-
 
     }
 }
